fb333173fb5f3f2ac72faf100817285db00cec4b
dmossing <mossing@berkeley.edu>
2018-05-29 14:03:06 -0700

    new vsn. of eye tracking code -- just binarize

--- track_eyes.m
+++ track_eyes.m
@@ -1,74 +1,101 @@
+% function [ctr,area] = track_eyes(foldname,rad_range)
+% cutoff = 10;
+% softmax_scale = 5e-3;
+% d = dir([foldname '/*.tiff']);
+% d = d(1:1000);
+% frameno = numel(d);
+% idxframe = zeros(frameno,1);
+% for i=1:numel(d)
+%     s = strsplit(d(1).name,'_');
+%     s = strsplit(s{end},'.tiff');
+%     s = s{1};
+%     idxframe(i) = str2num(s);
+% end
+% ctr = zeros(frameno,2);
+% area = zeros(frameno,1);
+% for i=1:frameno
+%     i
+%     if i==76
+%         disp('here')
+%     end
+%     data = imread([foldname '/' d(i).name]);
+%     [center,radii,metric] = imfindcircles(squeeze(data),rad_range,'Sensitivity',1);
+%     if(isempty(center))
+%         ctr(i,:) = [NaN NaN]; % could not find anything...
+%         area(i) = NaN;
+%     else
+%         wt = softmax(metric(1:cutoff),softmax_scale); % pick the circle with best score
+%         ctr(i,:) = sum(repmat(wt,1,2).*center(1:cutoff,:));
+%         area(i) = 4*pi*sum(wt.*radii(1:cutoff))^2;
+%     end
+% end
+% ctr(idxframe,:) = ctr;
+% area(idxframe) = area;
+%
+% function xstar = softmax(x,const)
+% wt = exp(x/const);
+% xstar = wt/sum(wt);
+
 function [ctr,area] = track_eyes(foldname,rad_range)
 d = dir([foldname '/*.tiff']);
+% cutoff = 100;
 frameno = numel(d);
 idxframe = zeros(frameno,1);
 for i=1:numel(d)
-    s = strsplit(d(1).name,'_');
+    s = strsplit(d(i).name,'_');
     s = strsplit(s{end},'.tiff');
     s = s{1};
     idxframe(i) = str2num(s);
 end
 ctr = zeros(frameno,2);
 area = zeros(frameno,1);
-parfor i=1:frameno
+img = imread([foldname '/' d(1).name]);
+figure;
+imshow(img)
+h = impoly;
+msk = h.createMask;
+% hg = hist(img(msk),0:255);
+% figure;
+% plot(hg)
+% cutoff_brightness = input('where to cut?')
+% parfor i=1:frameno
+%     i
+%     data = imread([foldname '/' d(i).name]);
+%     data = imbinarize(data);
+%     data(~msk) = 0;
+% %     data(data<cutoff_brightness) = 0;
+%     [center,radii,metric] = imfindcircles(squeeze(data),rad_range,'Sensitivity',1,'ObjectPolarity','bright');
+%     if(isempty(center))
+%         ctr(i,:) = [NaN NaN]; % could not find anything...
+%         area(i) = NaN;
+%     else
+% %         metric = metric(1:cutoff);
+% %         center = center(1:cutoff,:);
+% %         radius = radius(1:cutoff);
+%         gd = msk(sub2ind(size(msk),round(center(:,2)),round(center(:,1))));
+%         idx = find(gd,1);
+% %         [~,idx] = max(metric(gd)); % pick the circle with best score
+%         ctr(i,:) = center(idx,:);
+%         area(i) = 4*pi*radii(idx)^2;
+%     end
+% end
+% ctr(idxframe,:) = ctr;
+% area(idxframe) = area;
+
+for i=1:frameno
     i
     data = imread([foldname '/' d(i).name]);
-    [center,radii,metric] = imfindcircles(squeeze(data),rad_range,'Sensitivity',1);
-    if(isempty(center))
-        ctr(i,:) = [NaN NaN]; % could not find anything...
-        area(i) = NaN;
-    else
-        [~,idx] = max(metric); % pick the circle with best score
-        ctr(i,:) = center(idx,:);
-        area(i) = 4*pi*radii(idx)^2;
-    end
+    data = imbinarize(data);
+    data(~msk) = 0;
+    [xx,yy] = meshgrid(1:size(data,2),1:size(data,1));
+    distfun = @(x,y,x0,y0)((x-x0).^2+(y-y0).^2);
+    circfun = @(x0,y0,rad)(distfun(xx,yy,x0,y0)<rad^2);
+    L = @(x)(sum(sum(abs(data-circfun(x(1),x(2),x(3))))));
+    Nwhite = sum(data(:));
+    x0 = sum((xx(:).*data(:)))/Nwhite;
+    y0 = sum((yy(:).*data(:)))/Nwhite;
+    ctr(i,:) = [x0 y0];
+    area(i) = Nwhite;
 end
 ctr(idxframe,:) = ctr;
 area(idxframe) = area;
-
-% %%
-% img = zeros(256,320,numel(d),'uint8');
-% for i=1:size(img,3)
-%     img(:,:,i) = imread(d(i).name);
-%     i
-% end
-% 
-% %%
-% data = img;
-% rad_range = [30 50];
-% 
-% data = squeeze(data); % the raw images...
-% xc = size(data,2)/2; % image center
-% yc = size(data,1)/2;
-% 
-% warning off;
-% 
-% for(n=1:size(data,3))
-%     n
-%     [center,radii,metric] = imfindcircles(squeeze(data(:,:,n)),rad_range,'Sensitivity',1);
-%     if(isempty(center))
-%         eye(n).Centroid = [NaN NaN]; % could not find anything...
-%         eye(n).Area = NaN;
-%     else
-%         [~,idx] = max(metric); % pick the circle with best score
-%         eye(n).Centroid = center(idx,:);
-%         eye(n).Area = 4*pi*radii(idx)^2;
-%     end
-% end
-% 
-% %%
-% ctr = zeros(numel(eye),2);
-% area = zeros(numel(eye),1);
-% for i=1:size(ctr,1)
-%     ctr(i,:) = eye(i).Centroid;
-%     area(i) = eye(i).Area;
-% end
-% 
-% %%
-% for i=1:size(data,3)
-%     imshow(data(:,:,i))
-%     hold on
-%     plot(eye(i).Centroid(1),eye(i).Centroid(2),'m+')
-%     hold off
-%     pause
-% end
