89cd8f3b988448273f5a8b3544d09fe31e7827e9
Dan Mossing <dan@Dans-MacBook-Pro.local>
2020-04-02 14:54:27 -0700

    systematically exclude non-number folders from search during eye tracking

--- fold_fold_track_eyes_kmeans_convex.m
+++ fold_fold_track_eyes_kmeans_convex.m
@@ -27,7 +27,7 @@ d = dir(basefold);
 forbidden = {'.','..','Duplicate','.DS_Store','new_tree'};
 for i=1:numel(d)
     foldname = d(i).name;
-    if ~isnan(str2double(foldname)) %ismember(foldname,forbidden)
+    if ~ismember(foldname,forbidden)
         fold_track_eyes_kmeans_convex([basefold '/' foldname],dry_run)
         fold_smooth_eye_tracks([basefold '/' foldname],dry_run)
     end
