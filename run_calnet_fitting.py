fbd89c4c1ab0defddef9604365d7487a16be2bc5
Daniel Patrick Mossing <mossing@berkeley.edu>
2020-07-03 16:21:49 -0700

    current working(?) vsn of code on cluster

--- run_calnet_fitting.py
+++ run_calnet_fitting.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
-import fit_calnet_ori_with_optogenetics as fcowo
+import calnet.fit_calnet_ori_with_optogenetics as fcowo
 import sys
 
-ca_data_file = '/Users/dan/Documents/notebooks/mossing-PC/simulation/rs_vm_denoise_200605.npy'
-opto_data_file = '/Users/dan/Documents/notebooks/mossing-PC/shared_data/vip_halo_data_for_sim.npy'
+with open('~/adesnal/path_locations.txt','r') as f:
+    path_locs = f.read().splitlines()
+for loc in path_locs:    
+    print(loc)
+    sys.path.insert(0,loc)
+
+calnet_data_fold = '/global/scratch/mossing/calnet_data/'
+
+ca_data_file = calnet_data_fold+'rs_vm_denoise_200605.npy'
+opto_data_file = calnet_data_fold+'vip_halo_data_for_sim.npy'
 
 if __name__=="__main__":
-    ntries = int(sys.argv[1])
+    weight_base = sys.argv[1]
+    ntries = int(sys.argv[2])
 
-    weights_files = ['/Users/dan/Documents/notebooks/mossing-PC/shared_data/weights_200703_%d.npy'%itry for itry in range(ntries)]
+    weights_files = [calnet_data_fold+'weights/'+weight_base+'_%03d.npy'%itry for itry in range(ntries)]
 
     for itry in range(ntries):
         fcowo.fit_weights_and_save(weights_files[itry],ca_data_file=ca_data_file,opto_data_file=opto_data_file)
