4f77f969660d7c4f21d3228b14a824d932e014ad
Dan Mossing <dan.mossing@gmail.com>
2020-11-20 20:02:51 -0800

    debugging

--- calnet/fit_calnet_2step_with_tight_nonlinear_bidi_optogenetics.py
+++ calnet/fit_calnet_2step_with_tight_nonlinear_bidi_optogenetics.py
@@ -311,6 +311,7 @@ def fit_weights_and_save(weights_file,ca_data_file='rs_vm_denoise_200605.npy',op
             Ypc_list[iS][iT] = [None for icelltype in range(1,ncelltypes)]
             for icelltype in range(1,ncelltypes):
                 rss = Rso[icelltype][iS][iT].copy()#/mx[iS][icelltype] #.reshape(Rs[icelltype][ialign].shape[0],-1)
+                print('sum of isnan: '+str(np.isnan(rss).sum(1)))
                 #rss = Rso[icelltype][iS][iT].copy() #.reshape(Rs[icelltype][ialign].shape[0],-1)
                 rss = rss[np.isnan(rss).sum(1)==0]
         #         print(rss.max())
@@ -323,6 +324,7 @@ def fit_weights_and_save(weights_file,ca_data_file='rs_vm_denoise_200605.npy',op
     #                 print(np.min(np.sum(rs[icelltype][iS][iT],axis=1)))
                 except:
                     print('nope on Y')
+                    print('shape of rss: '+str(rss.shape))
                     print('mean of rss: '+str(np.mean(np.isnan(rss))))
                     print('min of this rs: '+str(np.min(np.sum(rs[icelltype][iS][iT],axis=1))))
             Yhat[iS][iT] = np.concatenate(y,axis=1)
